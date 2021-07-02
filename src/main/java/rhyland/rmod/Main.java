dd27625db606daf87bc86fc9c085ad58ddecfbf9
TaurusXin <zyx457601@gmail.com>
2021-07-02 16:10:48 +0800

    release: 1.1.0.2 tag

--- src/main/java/rhyland/rmod/Main.java
+++ src/main/java/rhyland/rmod/Main.java
@@ -13,7 +13,7 @@ public class Main
 {
     public static final String MODID = "rmod";
     public static final String NAME = "Rhyland Mod";
-    public static final String VERSION = "1.1.0.1";
+    public static final String VERSION = "1.1.0.2";
 
     @SidedProxy(clientSide = "rhyland.rmod.client.ClientProxy",
             serverSide = "rhyland.rmod.common.CommonProxy")
